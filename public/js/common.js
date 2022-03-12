26044c26549ccae36c6a74a5edacc01c2bd8feab
Pavel <wol1414@gmail.com>
2022-03-12 20:44:45 +0300

    update package

--- public/js/common.js
+++ public/js/common.js
@@ -151,7 +151,7 @@ const JSCCommon = {
 		// mask for input
 		let InputTel = [].slice.call(document.querySelectorAll('input[type="tel"]'));
 		InputTel.forEach(element => element.setAttribute("pattern", "[+][0-9]{1}[(][0-9]{3}[)][0-9]{3}-[0-9]{2}-[0-9]{2}"));
-		Inputmask("+9(999)999-99-99").mask(InputTel);
+		Inputmask({"mask":"+9(999)999-99-99", showMaskOnHover: false}).mask(InputTel);
 	},
 	// /inputMask
 	ifie() {
